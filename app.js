98972e09cb1e06f7c80016cd44390c9666ef33ab
Mouad <mouad.khchich@gmail.com>
2018-09-19 18:02:18 +0100

    More fixes for heroku

--- app.js
+++ app.js
@@ -23,6 +23,11 @@ mongoose.connect(keys.mongoURI);
 
 require("./routes/authRoutes")(app);
 require("./routes/dataRoutes")(app);
+app.use(express.static(path.join(__dirname, "client/build")));
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
